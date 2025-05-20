b37d20c44d89198b7e20401e9c0d76d1fdbe2266
JustNeku <dennisbruening96@hotmail.com>
2025-05-20 19:51:35 +0200

    🚀 Initial setup

---
+++ src/test/java/de/hgdb/bloggr/BloggrApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hgdb.bloggr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
